9f8aa9210b7103e5cd96f812bfa38baea04ce23c
Sohel Rana <rana72989@gmail.com>
2021-11-24 23:16:16 +0600

    aded

--- src/pages/Login/Firebase/firebase.config.js
+++ src/pages/Login/Firebase/firebase.config.js
@@ -5,13 +5,5 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-
-
-    // apiKey: "AIzaSyCME124VzWVrkjXK_3Qb9TburCLTABOJ1g",
-    // authDomain: "shaving-foam.firebaseapp.com",
-    // projectId: "shaving-foam",
-    // storageBucket: "shaving-foam.appspot.com",
-    // messagingSenderId: "578290093070",
-    // appId: "1:578290093070:web:29f950e18b7fa9fb79405f"
 };
 export default firebaseConfig;
